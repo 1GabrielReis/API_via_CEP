30303c6be8c02f801a7472b5c485d741e3b2ab0a
Gabriel <SenaibackEnd.gabriel@gmail.com>
2024-09-05 16:50:18 -0300

    criação da pagina web

---
+++ script/programa.js
@@ -0,0 +1,25 @@
+function consultarCEP(cep) {
+    if (cep.length === 8) { // Verifica se o CEP tem 8 dígitos
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => preencherCampos(data))
+        .catch(error => console.error('Erro:', error));
+    }
+  }
+
+  
+
+  function preencherCampos(dados) {
+    document.getElementById('logradouro').value = dados.logradouro || '';
+    document.getElementById('bairro').value = dados.bairro || '';
+    document.getElementById('cidade').value = dados.localidade || '';
+    document.getElementById('estado').value = dados.uf || '';
+  }
+
+  function limpCampos(){
+    document.getElementById('logradouro').value = "";
+    document.getElementById('bairro').value = "";
+    document.getElementById('cidade').value = "";
+    document.getElementById('estado').value = "";
+  }
+
